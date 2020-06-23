315203e4fcbddbd461297b5e77d1c3ac164eb01c
lnw920 <67312666+lnw920@users.noreply.github.com>
2020-06-23 16:24:17 +0800

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.sy7;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.health.PackageHealthStats;
+import android.provider.ContactsContract;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView)findViewById(R.id.list_item);
+
+        ArrayList<HashMap<String,String>> readContact =readContact();
+
+        listView.setAdapter(new SimpleAdapter(this,readContact,
+                R.layout.item,new String[]{"name","number"},new int[]{R.id.name,R.id.number}));
+
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{
+                    Manifest.permission.READ_CONTACTS},1);
+        }
+        else{
+            readContact();
+        }
+
+    }
+
+    private ArrayList<HashMap<String,String>> readContact() {
+        Uri rawContactsUri = Uri
+                .parse("content://com.android.contacts/raw_contacts");
+        Uri dataUri = Uri.parse("content://com.android.contacts/data");
+
+        ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+
+        // 从raw_contacts中读取联系人的id("contact_id")
+        Cursor rawContactsCursor = getContentResolver().query(rawContactsUri,
+                new String[] { "contact_id" }, null, null, null);
+        if (rawContactsCursor != null) {
+            while (rawContactsCursor.moveToNext()) {
+                String contactId = rawContactsCursor.getString(0);
+                // System.out.println(contactId);
+
+                // 根据contact_id从data表中查询出相应的电话号码和联系人名称, 实际上查询的是视图view_data
+                Cursor dataCursor = getContentResolver().query(dataUri,
+                        new String[] { "data1", "mimetype" }, "contact_id=?",
+                        new String[] { contactId }, null);
+
+                if (dataCursor != null) {
+                    HashMap<String, String> map = new HashMap<String, String>();
+                    while (dataCursor.moveToNext()) {
+                        String data1 = dataCursor.getString(0);
+                        String mimetype = dataCursor.getString(1);
+                        // System.out.println(contactId + ";" + data1 + ";"
+                        // + mimetype);
+                        if ("vnd.android.cursor.item/phone_v2".equals(mimetype)) {
+                            map.put("number", data1);
+                        } else if ("vnd.android.cursor.item/name"
+                                .equals(mimetype)) {
+                            map.put("name", data1);
+                        }
+                    }
+
+                    list.add(map);
+                    dataCursor.close();
+                }
+            }
+
+            rawContactsCursor.close();
+        }
+
+        return list;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:{
+                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    readContact();
+                }
+                else {
+                    Toast.makeText(this,"你被拒绝了",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+            default:
+                break;
+        }
+    }
+}
+
+
+
